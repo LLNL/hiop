ac76c32aa23fc2e3fef6f5497d901dc83bdc3731
Cosmin G. Petra <petra1@llnl.gov>
2017-01-17 17:31:17 -0800

    added a bit more doc and fixed typos

--- Optimization/hiopDualsUpdater.hpp
+++ Optimization/hiopDualsUpdater.hpp
@@ -8,18 +8,18 @@ public:
   virtual ~hiopDualUpdater() {};
 
   /* The method is called after each iteration to update the duals. Implementations for different 
-   * multiplier updating strategy are provided by child classes *
+   * multiplier updating strategies are provided by child classes 
    * - linear (Newton) update in hiopDualsNewtonLinearUpdate
    * - lsq in hiopDualsLsqUpdate
    * The parameters are:
-   * - iter_plus: [in/out] on return the duals should be update; primals are already updated, but 
+   * - iter_plus: [in/out] on return the duals should be updated; primals are already updated, but 
    * the function should not rely on this.
    * - iter: incumbent iterate that is going to be updated with iter_plus by the caller of this method.
-   * - f,c,d: fcn evals at the updated (primal) iteration
-   * - grad_f, jac_c, jac_d: derivatives at the  updated (primal) iteration
+   * - f,c,d: fcn evals at  iter_plus 
+   * - grad_f, jac_c, jac_d: derivatives at iter_plus
    * - search_dir: search direction (already used to update primals, potentially to be used to 
-   * update duals)
-   * - alpha_primal: step taken for primals (also taken for eq. duals for the linear duals update)
+   * update duals (in linear update))
+   * - alpha_primal: step taken for primals (also taken for eq. duals for the linear Newton duals update)
    * - alpha_dual: max step for the duals based on the fraction-to-the-boundary rule (not used
    * by lsq update)
    */
@@ -60,8 +60,9 @@ public:
   hiopDualsNewtonLinearUpdate(hiopNlpFormulation* nlp) : hiopDualUpdater(nlp) {};
   virtual ~hiopDualsNewtonLinearUpdate() {};
 
-  /* simple linear update of step length alpha_primal in eq. duals yc and yd and step length
-   * alpha_dual in the (signed or bounds) duals zl, zu, vl, and vu 
+  /* Linear update of step length alpha_primal in eq. duals yc and yd and step length
+   * alpha_dual in the (signed or bounds) duals zl, zu, vl, and vu.
+   * This is standard in (full) Newton IPMs. Very cheap!
    */
   virtual bool go(const hiopIterate& iter, hiopIterate& iter_plus,
 		  const double& f, const hiopVector& c, const hiopVector& d,
