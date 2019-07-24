5d482aa0120e0e870f7789a6c797a3a2649a4930
Juraj Kardos <juraj.kardos@usi.ch>
2019-07-24 16:39:58 -0700

    minor fix

--- src/Optimization/hiopResidualAugLagr.cpp
+++ src/Optimization/hiopResidualAugLagr.cpp
@@ -5,7 +5,7 @@ namespace hiop
 
 /* residual printing function - calls hiopVector::print 
  * prints up to max_elems (by default all), on rank 'rank' (by default on all) */
-virtual void hiopResidualAugLagr::print(FILE*, const char* msg=NULL, int max_elems=-1, int rank=-1) const
+void hiopResidualAugLagr::print(FILE* f, const char* msg, int max_elems, int rank) const
 {
 }
 
