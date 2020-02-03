ff3882fa0414b2996ba9336f1166976753678ad2
Cosmin G. Petra <petra1@llnl.gov>
2020-02-03 08:08:50 -0800

    fixed printf integer types mismatch warning

--- src/LinAlg/hiopMatrixSparseTriplet.cpp
+++ src/LinAlg/hiopMatrixSparseTriplet.cpp
@@ -382,19 +382,19 @@ void hiopMatrixSparseTriplet::print(FILE* file, const char* msg/*=NULL*/,
 				    int rank/*=-1*/) const 
 {
   int myrank=0, numranks=1; //this is a local object => always print
-
-    int max_elems = maxRows>=0 ? maxRows : nnz;
-    max_elems = std::min(max_elems, nnz);
-
+  
+  int max_elems = maxRows>=0 ? maxRows : nnz;
+  max_elems = std::min(max_elems, nnz);
+  
   if(myrank==rank || rank==-1) {
 
     if(NULL==msg) {
       if(numranks>1)
         fprintf(file, "matrix of size %lld %lld and nonzeros %lld, printing %d elems (on rank=%d)\n", 
-		nrows, ncols, nnz, max_elems, myrank);
+		m(), n(), numberOfNonzeros(), max_elems, myrank);
       else
         fprintf(file, "matrix of size %lld %lld and nonzeros %lld, printing %d elems\n", 
-		nrows, ncols, nnz, max_elems);
+		m(), n(), numberOfNonzeros(), max_elems);
     } else {
       fprintf(file, "%s ", msg);
     }    
