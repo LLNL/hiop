9e19c3c43cadfa35048f70e4a9f42daf03e31b7b
Cosmin G. Petra <petra1@llnl.gov>
2020-05-01 18:14:45 -0700

    added example 5
    a take on a (problematically) non-convex model and degenerate (linear dependent constraints).
    It only creates non-convexity at this point

---
+++ src/Drivers/nlpMDS_ex5.hpp
@@ -0,0 +1,580 @@
+#ifndef HIOP_EXAMPLE_EX5
+#define HIOP_EXAMPLE_EX5
+
+#include "hiopInterface.hpp"
+
+//this include is not needed in general
+//we use hiopMatrixDense in this particular example for convienience
+#include "hiopMatrix.hpp" 
+
+#ifdef HIOP_USE_MPI
+#include "mpi.h"
+#else
+#define MPI_COMM_WORLD 0
+#define MPI_Comm int
+#endif
+
+#include <cassert>
+#include <cstring> //for memcpy
+#include <cstdio>
+#include <cmath>
+
+/* Problem test for the linear algebra of Mixed Dense-Sparse NLPs
+ *  min   - sum 0.5 {x_i*(x_{i}-1) : i=1,...,ns} - 0.5 y'*Qd*y + 0.5 s^T s
+ *  s.t.  x+s + Md y = 0, i=1,...,ns
+ *        [-2  ]    [ x_1 + e^T s]   [e^T]      [ 2 ]
+ *        [-inf] <= [ x_2        ] + [e^T] y <= [ 2 ]
+ *        [-2  ]    [ x_3        ]   [e^T]      [inf]
+ *        -10 <= x <= 3
+ *        s>=0
+ *        -4 <=y <=4, the rest of y are free
+ *       
+ *
+ * 
+ * The vector 'y' is of dimension nd = ns (can be changed in the constructor)
+ * Dense matrices Qd and Md are such that
+ * Qd  = two on the diagonal, one on the first offdiagonals, zero elsewhere
+ * Md  = minus one everywhere
+ * e   = vector of all ones
+ *
+ * Coding of the problem in MDS HiOp input: order of variables need to be [x,s,y] 
+ * since [x,s] are the so-called sparse variables and y are the dense variables
+ */
+class Ex5 : public hiop::hiopInterfaceMDS
+{
+public:
+  Ex5(int ns_)
+    : Ex5(ns_, ns_)
+  {
+  }
+  
+  Ex5(int ns_, int nd_)
+    : ns(ns_)
+  {
+    if(ns<0) {
+      ns = 0;
+    } else {
+      if(4*(ns/4) != ns) {
+	ns = 4*((4+ns)/4);
+	printf("[warning] number (%d) of sparse vars is not a multiple ->was altered to %d\n", 
+	       ns_, ns); 
+      }
+    }
+
+    if(nd_<0) nd=0;
+    else nd = nd_;
+
+    Q  = new hiop::hiopMatrixDense(nd,nd);
+    Q->setToConstant(0.);
+    Q->addDiagonal(-2.);
+    double** Qa = Q->get_M();
+    for(int i=1; i<nd-1; i++) {
+      Qa[i][i+1] = 1.;
+      Qa[i+1][i] = 1.;
+    }
+
+    Md = new hiop::hiopMatrixDense(ns,nd);
+    Md->setToConstant(-1.0);
+
+    _buf_y = new double[nd];
+
+    haveIneq = true;
+  }
+
+  virtual ~Ex5()
+  {
+    delete[] _buf_y;
+    delete Md;
+    delete Q;
+  }
+  
+  bool get_prob_sizes(long long& n, long long& m)
+  { 
+    n=2*ns+nd;
+    m=ns+3*haveIneq; 
+    return true; 
+  }
+
+  bool get_vars_info(const long long& n, double *xlow, double* xupp, NonlinearityType* type)
+  {
+    //assert(n>=4 && "number of variables should be greater than 4 for this example");
+    assert(n==2*ns+nd);
+
+    //x
+    for(int i=0; i<ns; ++i) xlow[i] = -10.;
+    //s
+    for(int i=ns; i<2*ns; ++i) xlow[i] = 0.;
+    //y 
+    for(int i=2*ns; i<n; ++i) xlow[i] = -4.;
+    
+    //x
+    for(int i=0; i<ns; ++i) xupp[i] = 3.;
+    //s
+    for(int i=ns; i<2*ns; ++i) xupp[i] = +1e+20;
+    //y
+    xupp[2*ns] = 4.;
+    for(int i=2*ns+1; i<n; ++i) xupp[i] = 4.;
+
+    for(int i=0; i<n; ++i) type[i]=hiopNonlinear;
+    return true;
+  }
+
+  bool get_cons_info(const long long& m, double* clow, double* cupp, NonlinearityType* type)
+  {
+    assert(m==ns+3*haveIneq);
+    int i;
+    //x+s - Md y = 0, i=1,...,ns
+    for(i=0; i<ns; i++) clow[i] = cupp[i] = 0.;
+
+    if(haveIneq) {
+      // [-2  ]    [ x_1 + e^T s]   [e^T]      [ 2 ]
+      clow[i] = -2; cupp[i++] = 2.;
+      // [-inf] <= [ x_2        ] + [e^T] y <= [ 2 ]
+      clow[i] = -1e+20; cupp[i++] = 2.;
+      // [-2  ]    [ x_3        ]   [e^T]      [inf]
+      clow[i] = -2; cupp[i++] = 1e+20;
+    }
+    assert(i==m);
+
+    for(i=0; i<m; ++i) type[i]=hiopNonlinear;
+    return true;
+  }
+
+  bool get_sparse_dense_blocks_info(int& nx_sparse, int& nx_dense,
+				    int& nnz_sparse_Jace, int& nnz_sparse_Jaci,
+				    int& nnz_sparse_Hess_Lagr_SS, int& nnz_sparse_Hess_Lagr_SD)
+  {
+    nx_sparse = 2*ns;
+    nx_dense = nd;
+    nnz_sparse_Jace = 2*ns;
+    nnz_sparse_Jaci = (ns==0 || !haveIneq) ? 0 : 3+ns;
+    nnz_sparse_Hess_Lagr_SS = 2*ns;
+    nnz_sparse_Hess_Lagr_SD = 0.;
+    return true;
+  }
+
+  bool eval_f(const long long& n, const double* x, bool new_x, double& obj_value)
+  {
+    //assert(ns>=4);
+    assert(Q->n()==nd); assert(Q->m()==nd);
+    obj_value=0.;//x[0]*(x[0]-1.);
+    //sum 0.5 {x_i*(x_{i}-1) : i=1,...,ns} + 0.5 y'*Qd*y + 0.5 s^T s
+    for(int i=0; i<ns; i++) obj_value -= x[i]*(x[i]-1.);
+    obj_value *= 0.5;
+
+    double term2=0.;
+    const double* y = x+2*ns;
+    Q->timesVec(0.0, _buf_y, 1., y);
+    for(int i=0; i<nd; i++) term2 += _buf_y[i] * y[i];
+    obj_value += 0.5*term2;
+    
+    const double* s=x+ns;
+    double term3=0.;//s[0]*s[0];
+    for(int i=0; i<ns; i++) term3 += s[i]*s[i];
+    obj_value += 0.5*term3;
+
+    return true;
+  }
+
+  virtual bool eval_cons(const long long& n, const long long& m, 
+			 const long long& num_cons, const long long* idx_cons,  
+			 const double* x, bool new_x, double* cons)
+  {
+    const double* s = x+ns;
+    const double* y = x+2*ns;
+
+    assert(num_cons==ns || num_cons==3*haveIneq);
+
+    bool isEq=false;
+    for(int irow=0; irow<num_cons; irow++) {
+      const int con_idx = (int) idx_cons[irow];
+      if(con_idx<ns) {
+	//equalities: x+s - Md y = 0
+	cons[con_idx] = x[con_idx] + s[con_idx];
+	isEq=true;
+      } else if(haveIneq) {
+	assert(con_idx<ns+3);
+	//inequality
+	const int conineq_idx=con_idx-ns;
+	if(conineq_idx==0) {
+	  cons[conineq_idx] = x[0];
+	  for(int i=0; i<ns; i++) cons[conineq_idx] += s[i];
+	  for(int i=0; i<nd; i++) cons[conineq_idx] += y[i];
+
+	} else if(conineq_idx==1) {
+	  cons[conineq_idx] = x[1];
+	  for(int i=0; i<nd; i++) cons[conineq_idx] += y[i];
+	} else if(conineq_idx==2) {
+	  cons[conineq_idx] = x[2];
+	  for(int i=0; i<nd; i++) cons[conineq_idx] += y[i];
+	} else { assert(false); }
+      }  
+    }
+    if(isEq) {
+      Md->timesVec(1.0, cons, 1.0, y);
+    }
+    return true;
+  }
+  
+  //sum 0.5 {x_i*(x_{i}-1) : i=1,...,ns} + 0.5 y'*Qd*y + 0.5 s^T s
+  bool eval_grad_f(const long long& n, const double* x, bool new_x, double* gradf)
+  {
+    //! assert(ns>=4); assert(Q->n()==ns/4); assert(Q->m()==ns/4);
+    //x_i - 0.5 
+    for(int i=0; i<ns; i++) 
+      gradf[i] = -x[i]+0.5;
+
+    //Qd*y
+    const double* y = x+2*ns;
+    double* gradf_y = gradf+2*ns;
+    Q->timesVec(0.0, gradf_y, 1., y);
+
+    //s
+    const double* s=x+ns;
+    double* gradf_s = gradf+ns;
+    for(int i=0; i<ns; i++) gradf_s[i] = s[i];
+
+    return true;
+  }
+ 
+  virtual bool
+  eval_Jac_cons(const long long& n, const long long& m, 
+		const long long& num_cons, const long long* idx_cons,
+		const double* x, bool new_x,
+		const long long& nsparse, const long long& ndense, 
+		const int& nnzJacS, int* iJacS, int* jJacS, double* MJacS, 
+		double** JacD)
+  {
+    assert(num_cons==ns || num_cons==3*haveIneq);
+
+    if(iJacS!=NULL && jJacS!=NULL) {
+      int nnzit=0;
+      for(int itrow=0; itrow<num_cons; itrow++) {
+	const int con_idx = (int) idx_cons[itrow];
+	if(con_idx<ns && ns>0) {
+	  //sparse Jacobian eq w.r.t. x and s
+	  //x
+	  iJacS[nnzit] = con_idx;
+	  jJacS[nnzit] = con_idx;
+	  nnzit++;
+
+	  //s
+	  iJacS[nnzit] = con_idx;
+	  jJacS[nnzit] = con_idx+ns;
+	  nnzit++;
+
+	} else if(haveIneq) {
+	  //sparse Jacobian ineq w.r.t x and s
+	  if(con_idx-ns==0 && ns>0) {
+	    //w.r.t x_1
+	    iJacS[nnzit] = 0;
+	    jJacS[nnzit] = 0;
+	    nnzit++;
+	    //w.r.t s
+	    for(int i=0; i<ns; i++) {
+	      iJacS[nnzit] = 0;
+	      jJacS[nnzit] = ns+i;
+	      nnzit++;
+	    }
+	  } else {
+	    if( (con_idx-ns==1 || con_idx-ns==2) && ns>0 ) {
+	      //w.r.t x_2 or x_3
+	      iJacS[nnzit] = con_idx-ns;
+	      jJacS[nnzit] = con_idx-ns;
+	      nnzit++;
+	    }
+	  }
+	}
+      }
+      assert(nnzit==nnzJacS);
+    } 
+    //values for sparse Jacobian if requested by the solver
+    if(MJacS!=NULL) {
+     int nnzit=0;
+     for(int itrow=0; itrow<num_cons; itrow++) {
+       const int con_idx = (int) idx_cons[itrow];
+       if(con_idx<ns && ns>0) {
+	 //sparse Jacobian EQ w.r.t. x and s
+	 //x
+	 MJacS[nnzit] = 1.;
+	 nnzit++;
+	 
+	 //s
+	 MJacS[nnzit] = 1.;
+	 nnzit++;
+	 
+       } else if(haveIneq) {
+	 //sparse Jacobian INEQ w.r.t x and s
+	 if(con_idx-ns==0 && ns>0) {
+	   //w.r.t x_1
+	   MJacS[nnzit] = 1.;
+	   nnzit++;
+	   //w.r.t s
+	   for(int i=0; i<ns; i++) {
+	     MJacS[nnzit] = 1.;
+	     nnzit++;
+	   }
+	 } else {
+	   if( (con_idx-ns==1 || con_idx-ns==2) && ns>0) {
+	     //w.r.t x_2 or x_3
+	     MJacS[nnzit] = 1.;
+	     nnzit++;
+	   }
+	 }
+       }
+     }
+     assert(nnzit==nnzJacS);
+    }
+    
+    //dense Jacobian w.r.t y
+    if(JacD!=NULL) {
+      bool isEq=false;
+      for(int itrow=0; itrow<num_cons; itrow++) {
+	const int con_idx = (int) idx_cons[itrow];
+	if(con_idx<ns) {
+	  isEq=true;
+	  assert(num_cons==ns);
+	  continue;
+	} else if(haveIneq) {
+	  //do an in place fill-in for the ineq Jacobian corresponding to e^T
+	  assert(con_idx-ns==0 || con_idx-ns==1 || con_idx-ns==2);
+	  assert(num_cons==3);
+	  for(int i=0; i<nd; i++) {
+	    JacD[con_idx-ns][i] = 1.;
+	  }
+	}
+      }
+      if(isEq) {
+	memcpy(JacD[0], Md->local_buffer(), ns*nd*sizeof(double));
+      }
+    }
+
+    return true;
+  }
+ 
+  bool eval_Hess_Lagr(const long long& n, const long long& m, 
+			      const double* x, bool new_x, const double& obj_factor,
+			      const double* lambda, bool new_lambda,
+			      const long long& nsparse, const long long& ndense, 
+			      const int& nnzHSS, int* iHSS, int* jHSS, double* MHSS, 
+			      double** HDD,
+			      int& nnzHSD, int* iHSD, int* jHSD, double* MHSD)
+  {
+    //Note: lambda is not used since all the constraints are linear and, therefore, do 
+    //not contribute to the Hessian of the Lagrangian
+
+    assert(nnzHSS==2*ns);
+    assert(nnzHSD==0);
+    assert(iHSD==NULL); assert(jHSD==NULL); assert(MHSD==NULL);
+
+    if(iHSS!=NULL && jHSS!=NULL) {
+      for(int i=0; i<2*ns; i++) iHSS[i] = jHSS[i] = i;     
+    }
+
+    if(MHSS!=NULL) {
+      for(int i=0; i<ns; i++) MHSS[i] = -obj_factor;
+      for(int i=ns; i<2*ns; i++) MHSS[i] = obj_factor;
+    }
+
+    if(HDD!=NULL) {
+      const int nx_dense_squared = nd*nd;
+      //memcpy(HDD[0], Q->local_buffer(), nx_dense_squared*sizeof(double));
+      const double* Qv = Q->local_buffer();
+      for(int i=0; i<nx_dense_squared; i++)
+	HDD[0][i] = obj_factor*Qv[i];
+    }
+    return true;
+  }
+  
+  bool get_starting_point(const long long& global_n, double* x0)
+  {
+    assert(global_n==2*ns+nd); 
+    for(int i=0; i<global_n; i++) x0[i]=10.;
+    return true;
+  }
+
+protected:
+  int ns, nd;
+  hiop::hiopMatrixDense *Q, *Md;
+  double* _buf_y;
+  bool haveIneq;
+};
+
+class Ex4OneCallCons : public Ex5
+{
+public:
+  Ex4OneCallCons(int ns_in)
+    : Ex5(ns_in)
+  {
+  }
+  
+  Ex4OneCallCons(int ns_in, int nd_in)
+    : Ex5(ns_in, nd_in)
+  {
+  }
+  
+  virtual ~Ex4OneCallCons()
+  {
+  }
+
+  bool eval_cons(const long long& n, const long long& m, 
+		 const long long& num_cons, const long long* idx_cons,  
+		 const double* x, bool new_x, double* cons)
+  {
+    //return false so that HiOp will rely on the on-call constraint evaluator defined below
+    return false;
+  }
+  /** all constraints evaluated in here */
+  bool eval_cons(const long long& n, const long long& m, 
+		 const double* x, bool new_x, double* cons)
+  {
+    assert(3*haveIneq+ns == m);
+    const double* s = x+ns;
+    const double* y = x+2*ns;
+
+    for(int con_idx=0; con_idx<m; ++con_idx) {
+      if(con_idx<ns) {
+	//equalities
+	cons[con_idx] = x[con_idx]+s[con_idx];
+      } else if(haveIneq) {
+	//inequalties
+	assert(con_idx<ns+3);
+	if(con_idx==ns) {
+	  cons[con_idx] = x[0];
+	  for(int i=0; i<ns; i++) cons[con_idx] += s[i];
+	  for(int i=0; i<nd; i++) cons[con_idx] += y[i];
+
+	} else if(con_idx==ns+1) {
+	  cons[con_idx] = x[1];
+	  for(int i=0; i<nd; i++) cons[con_idx] += y[i];
+	} else if(con_idx==ns+2) {
+	  cons[con_idx] = x[2];
+	  for(int i=0; i<nd; i++) cons[con_idx] += y[i];
+	} else { assert(false); }
+      }
+    }
+
+    // apply Md to y and add the result to equality part of 'cons'
+
+    //we know that equalities are the first ns constraints so this should work
+    Md->timesVec(1.0, cons, 1.0, y);
+    return true;
+  }
+
+  virtual bool
+  eval_Jac_cons(const long long& n, const long long& m, 
+		const long long& num_cons, const long long* idx_cons,
+		const double* x, bool new_x,
+		const long long& nsparse, const long long& ndense, 
+		const int& nnzJacS, int* iJacS, int* jJacS, double* MJacS, 
+		double** JacD)
+  {
+    return false; // so that HiOp will call the one-call full-Jacob function below
+  }
+
+  virtual bool
+  eval_Jac_cons(const long long& n, const long long& m, 
+		const double* x, bool new_x,
+		const long long& nsparse, const long long& ndense, 
+		const int& nnzJacS, int* iJacS, int* jJacS, double* MJacS, 
+		double** JacD)
+  {
+    assert(m==ns+3*haveIneq);
+
+    if(iJacS!=NULL && jJacS!=NULL) {
+      int nnzit=0;
+      for(int con_idx=0; con_idx<ns; ++con_idx) {
+	//sparse Jacobian eq w.r.t. x and s
+	//x
+	iJacS[nnzit] = con_idx;
+	jJacS[nnzit] = con_idx;
+	nnzit++;
+	
+	//s
+	iJacS[nnzit] = con_idx;
+	jJacS[nnzit] = con_idx+ns;
+	nnzit++;
+      }
+      if(haveIneq && ns>0) {
+	for(int con_idx=ns; con_idx<m; ++con_idx) {
+
+	  //sparse Jacobian ineq w.r.t x and s
+	  if(con_idx==ns) {
+	    //w.r.t x_1
+	    iJacS[nnzit] = con_idx;
+	    jJacS[nnzit] = 0;
+	    nnzit++;
+	    //w.r.t s
+	    for(int i=0; i<ns; i++) {
+	      iJacS[nnzit] = con_idx;
+	      jJacS[nnzit] = ns+i;
+	      nnzit++;
+	    }
+	  } else {
+	    if(con_idx-ns==1 || con_idx-ns==2) {
+	      //w.r.t x_2 or x_3
+	      iJacS[nnzit] = con_idx;
+	      jJacS[nnzit] = con_idx-ns;
+	      nnzit++;
+	    } else { assert(false); }
+	  }
+	}
+      }
+      assert(nnzit==nnzJacS);
+    }
+    //values for sparse Jacobian if requested by the solver
+    if(MJacS!=NULL) {
+      int nnzit=0;
+      for(int con_idx=0; con_idx<ns; ++con_idx) {
+	//sparse Jacobian EQ w.r.t. x and s
+	//x
+	MJacS[nnzit] = 1.;
+	nnzit++;
+	
+	//s
+	MJacS[nnzit] = 1.;
+	nnzit++;
+	
+      }
+      
+      if(haveIneq && ns>0) {
+	for(int con_idx=ns; con_idx<m; ++con_idx) {
+	  //sparse Jacobian INEQ w.r.t x and s
+	  if(con_idx-ns==0) {
+	    //w.r.t x_1
+	    MJacS[nnzit] = 1.;
+	    nnzit++;
+	    //w.r.t s
+	    for(int i=0; i<ns; i++) {
+	      MJacS[nnzit] = 1.;
+	      nnzit++;
+	    }
+	  } else {
+	    if(con_idx-ns==1 || con_idx-ns==2) {
+	      //w.r.t x_2 or x_3
+	      MJacS[nnzit] = 1.;
+	      nnzit++;
+	    } else { assert(false); }
+	  }
+	}
+      }
+      assert(nnzit==nnzJacS);
+    }
+    
+    //dense Jacobian w.r.t y
+    if(JacD!=NULL) {
+      //just copy the dense Jacobian corresponding to equalities
+      memcpy(JacD[0], Md->local_buffer(), ns*nd*sizeof(double));
+      
+      if(haveIneq) {
+	assert(ns+3 == m);
+	//do an in place fill-in for the ineq Jacobian corresponding to e^T
+	for(int i=0; i<3*nd; ++i)
+	  JacD[ns][i] = 1.;
+      }
+    }
+    return true;
+  }
+};
+
+#endif
---
+++ src/Drivers/nlpMDS_ex5_driver.cpp
@@ -0,0 +1,157 @@
+#include "nlpMDS_ex5.hpp"
+#include "hiopNlpFormulation.hpp"
+#include "hiopAlgFilterIPM.hpp"
+
+#ifdef HIOP_USE_MAGMA
+#include "magma_v2.h"
+#endif
+
+#include <cstdlib>
+#include <string>
+
+using namespace hiop;
+
+static bool self_check(long long n, double obj_value);
+
+static bool parse_arguments(int argc, char **argv,
+			    bool& self_check,
+			    long long& n_sp,
+			    long long& n_de,
+			    bool& one_call_cons)
+{
+  self_check=false;
+  n_sp = 12;
+  n_de = 10;
+  one_call_cons = false;
+  switch(argc) {
+  case 1:
+    //no arguments
+    return true;
+    break;
+  case 5: // 4 arguments
+    {
+      if(std::string(argv[4]) == "-selfcheck")
+	self_check=true;
+    }
+  case 4: // 3 arguments
+    {
+      one_call_cons = (bool) atoi(argv[3]);
+    }
+  case 3: //2 arguments
+    {
+      n_de = atoi(argv[2]);
+      if(n_de<0) n_de = 0;
+    }
+  case 2: //1 argument
+    {
+      n_sp = atoi(argv[1]);
+      if(n_sp<0) n_sp = 0;
+    }
+    break;
+  default: 
+    return false; //5 or more arguments
+  }
+
+  if(self_check && n_sp!=400 && n_de!=100)
+    return false;
+  
+  return true;
+};
+
+static void usage(const char* exeName)
+{
+  printf("HiOp driver %s that solves a synthetic problem of variable size in the "
+	 "mixed dense-sparse formulation.\n", exeName);
+  printf("Usage: \n");
+  printf("  '$ %s sp_vars_size de_vars_size eq_ineq_combined_nlp -selfcheck'\n", exeName);
+  printf("Arguments, all integers, excepting string '-selfcheck'\n");
+  printf("  'sp_vars_size': # of sparse variables [default 400, optional]\n");
+  printf("  'de_vars_size': # of dense variables [default 100, optional]\n");
+  printf("  '-selfcheck': compares the optimal objective with sp_vars_size being 400 and "
+	 "de_vars_size being 100 (these two exact values must be passed as arguments). [optional]\n");
+  printf("  'eq_ineq_combined_nlp': 0 or 1, specifying whether the NLP formulation with split "
+	 "constraints should be used (0) or not (1) [default 0, optional]\n");
+}
+
+
+int main(int argc, char **argv)
+{
+  int rank=0;
+#ifdef HIOP_USE_MPI
+  MPI_Init(&argc, &argv);
+  int comm_size;
+  int ierr = MPI_Comm_size(MPI_COMM_WORLD, &comm_size); assert(MPI_SUCCESS==ierr);
+  //int ierr = MPI_Comm_rank(MPI_COMM_WORLD, &rank); assert(MPI_SUCCESS==ierr);
+  if(comm_size != 1) {
+    printf("[error] driver detected more than one rank but the driver should be run "
+	   "in serial only; will exit\n");
+    MPI_Finalize();
+    return 1;
+  }
+#endif
+
+#ifdef HIOP_USE_MAGMA
+  magma_init();
+#endif
+
+  bool selfCheck, one_call_cons;
+  long long n_sp, n_de;
+  if(!parse_arguments(argc, argv, selfCheck, n_sp, n_de, one_call_cons)) {
+    usage(argv[0]);
+    return 1;
+  }
+
+  double obj_value=-1e+20;
+  hiopSolveStatus status;
+
+  hiopInterfaceMDS* nlp_interface;
+  if(one_call_cons) {
+    nlp_interface = new Ex4OneCallCons(n_sp, n_de);
+  } else {
+    nlp_interface = new Ex5(n_sp, n_de);
+  }
+
+  hiopNlpMDS nlp(*nlp_interface);
+
+  nlp.options->SetStringValue("dualsUpdateType", "linear");
+  nlp.options->SetStringValue("dualsInitialization", "zero");
+
+  nlp.options->SetStringValue("Hessian", "analytical_exact");
+  nlp.options->SetStringValue("KKTLinsys", "xdycyd");
+  nlp.options->SetStringValue("compute_mode", "hybrid");
+
+  nlp.options->SetIntegerValue("verbosity_level", 3);
+  nlp.options->SetNumericValue("mu0", 1e-1);
+  hiopAlgFilterIPMNewton solver(&nlp);
+  status = solver.run();
+  obj_value = solver.getObjective();
+
+  delete nlp_interface;
+  
+  if(status<0) {
+    if(rank==0)
+      printf("solver returned negative solve status: %d (with objective is %18.12e)\n", status, obj_value);
+    return -1;
+  }
+
+  // this is used for testing when the driver is in '-selfcheck' mode
+  if(selfCheck) {
+    if(fabs(obj_value-(-4.999509728895e+01))>1e-6) {
+      printf("selfcheck: objective mismatch for Ex4 MDS problem with 400 sparse variables and 100 "
+	     "dense variables did. BTW, obj=%18.12e was returned by HiOp.\n", obj_value);
+      return -1;
+    }
+  } else {
+    if(rank==0) {
+      printf("Optimal objective: %22.14e. Solver status: %d\n", obj_value, status);
+    }
+  }
+#ifdef HIOP_USE_MAGMA
+  magma_finalize();
+#endif
+#ifdef HIOP_USE_MPI
+  MPI_Finalize();
+#endif
+
+  return 0;
+}
