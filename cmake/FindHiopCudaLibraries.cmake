66d4265abf356e8347d949fd396fc277a62c2a6c
Slaven Peles <slaven.peles@pnnl.gov>
2020-05-08 21:04:38 -0700

    Improvements to CMake configuration
    - Caches HiOp variables
    - Improved search, looks at LD_LIBRARY_PATH

--- cmake/FindHiopCudaLibraries.cmake
+++ cmake/FindHiopCudaLibraries.cmake
@@ -3,54 +3,50 @@
 
 Exports target `hiop_cuda` which finds all cuda libraries needed by hiop.
 
-Users may set the following variables:
-
-- HIOP_NVCC_ARCH
-- HIOP_CUDA_INCLUDE_DIR
-- HIOP_CUDA_LIB_DIR
-
 ]]
 
 add_library(hiop_cuda INTERFACE)
 
-if( ("${CMAKE_VERSION}" VERSION_EQUAL 3.8) OR ("${CMAKE_VERSION}" VERSION_GREATER 3.8) )
-  include(CheckLanguage)
-  enable_language(CUDA)
-  check_language(CUDA)
-else()
-  find_package(CUDA REQUIRED)
-endif()
+find_package(CUDA REQUIRED)
+
+find_library(CUDA_culibos_LIBRARY
+  NAMES
+  culibos
+  PATHS
+  ${CUDA_TOOLKIT_ROOT_DIR} $ENV{CUDA_TOOLKIT_ROOT_DIR}
+  PATH_SUFFIXES
+  lib64 lib
+)
+
+find_library(CUDA_cublasLt_LIBRARY
+  NAMES
+  cublasLt
+  PATHS
+  ${CUDA_TOOLKIT_ROOT_DIR} $ENV{CUDA_TOOLKIT_ROOT_DIR}
+  PATH_SUFFIXES
+  lib64 lib
+)
+
+find_library(CUDA_nvblas_LIBRARY
+  NAMES
+  nvblas
+  PATHS
+  ${CUDA_TOOLKIT_ROOT_DIR} $ENV{CUDA_TOOLKIT_ROOT_DIR}
+  PATH_SUFFIXES
+  lib64 lib
+)
 
 target_link_libraries(hiop_cuda INTERFACE
-  culibos cublas cublasLt nvblas cusparse cudart cudadevrt)
+  ${CUDA_cublas_LIBRARY}
+  ${CUDA_CUDART_LIBRARY}
+  ${CUDA_cudadevrt_LIBRARY}
+  ${CUDA_cusparse_LIBRARY}
+  ${CUDA_cublasLt_LIBRARY}
+  ${CUDA_nvblas_LIBRARY}
+  ${CUDA_culibos_LIBRARY}
+)
 
-if(NOT DEFINED CMAKE_CUDA_STANDARD)
-  set(CMAKE_CUDA_STANDARD 11)
-  set(CMAKE_CUDA_STANDARD_REQUIRED ON)
-endif()
-
-if(HIOP_NVCC_ARCH)
-  set(CMAKE_CUDA_FLAGS "-arch=${HIOP_NVCC_ARCH}")
-  message(STATUS "Using CUDA arch ${HIOP_NVCC_ARCH}")
-else()
-  set(CMAKE_CUDA_FLAGS "-arch=sm_35")
-  message(STATUS "Using CUDA arch sm_35")
-endif()
-set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} --expt-extended-lambda")
-
-if(HIOP_CUDA_LIB_DIR)
-  target_compile_options(hiop_cuda INTERFACE -L${HIOP_CUDA_LIB_DIR})
-endif(HIOP_CUDA_LIB_DIR)
-
-# on some systems RHEL Nvidia toolkit puts cuBlas in different locations
-# user can setup/overwrite the lib directory
-if(HIOP_CUBLAS_LIB_DIR)
-  target_compile_options(hiop_cuda INTERFACE -L${HIOP_CUBLAS_LIB_DIR})
-endif(HIOP_CUBLAS_LIB_DIR)
-
-if(HIOP_CUDA_INCLUDE_DIR)
-  target_include_directories(hiop_cuda INTERFACE ${HIOP_CUDA_INCLUDE_DIR})
-endif()
+target_include_directories(hiop_cuda INTERFACE ${CUDA_TOOLKIT_INCLUDE})
 
 # for now we rely on MAGMA for GPUs computations
 include(FindMagma)
--- cmake/FindUMFPACK.cmake
+++ cmake/FindUMFPACK.cmake
@@ -17,18 +17,23 @@ find_library(UMFPACK_LIBRARY
   umfpack
   PATHS
   ${UMFPACK_DIR} $ENV{UMFPACK_DIR} ${HIOP_UMFPACK_DIR}
+  ENV LD_LIBRARY_PATH ENV DYLD_LIBRARY_PATH
   PATH_SUFFIXES
-  lib)
+  lib64 lib)
+
+if(UMFPACK_LIBRARY)
+  get_filename_component(UMFPACK_LIBRARY_DIR ${UMFPACK_LIBRARY} DIRECTORY)
+endif()
 
 find_path(UMFPACK_INCLUDE_DIR
   NAMES
   umfpack.h
   PATHS
-  ${UMFPACK_DIR} $ENV{UMFPACK_DIR} ${HIOP_UMFPACK_DIR}
+  ${UMFPACK_DIR} $ENV{UMFPACK_DIR} ${HIOP_UMFPACK_DIR} ${UMFPACK_LIBRARY_DIR}/..
   PATH_SUFFIXES
   include
-  suitesparse
-  ufsparse)
+  include/suitesparse
+  include/ufsparse)
 
 set(UMFPACK_INCLUDE_DIR ${UMFPACK_INCLUDE_DIR} CACHE PATH "Path to umfpack.h")
 set(UMFPACK_LIBRARY ${UMFPACK_LIBRARY} CACHE PATH "Path to umfpack library")
@@ -37,6 +42,7 @@ if(UMFPACK_LIBRARY)
   add_library(UMFPACK INTERFACE)
   target_link_libraries(UMFPACK INTERFACE ${UMFPACK_LIBRARY})
   target_include_directories(UMFPACK INTERFACE ${UMFPACK_INCLUDE_DIR})
+  message(STATUS "Found UMFPACK library: ${UMFPACK_LIBRARY}")
 else()
   message(STATUS "UMFPACK was not found.")
 endif()
