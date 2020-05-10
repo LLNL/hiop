aed62c8d0f0e2d22ffc26607c16f29c5ace599aa
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-05-09 22:29:57 -0700

    whitespace fixes for consistent two-space indentation

--- cmake/FindHiopCudaLibraries.cmake
+++ cmake/FindHiopCudaLibraries.cmake
@@ -16,7 +16,7 @@ find_library(CUDA_culibos_LIBRARY
   ${CUDA_TOOLKIT_ROOT_DIR} $ENV{CUDA_TOOLKIT_ROOT_DIR}
   PATH_SUFFIXES
   lib64 lib
-)
+  )
 
 find_library(CUDA_cublasLt_LIBRARY
   NAMES
@@ -25,7 +25,7 @@ find_library(CUDA_cublasLt_LIBRARY
   ${CUDA_TOOLKIT_ROOT_DIR} $ENV{CUDA_TOOLKIT_ROOT_DIR}
   PATH_SUFFIXES
   lib64 lib
-)
+  )
 
 find_library(CUDA_nvblas_LIBRARY
   NAMES
@@ -34,7 +34,7 @@ find_library(CUDA_nvblas_LIBRARY
   ${CUDA_TOOLKIT_ROOT_DIR} $ENV{CUDA_TOOLKIT_ROOT_DIR}
   PATH_SUFFIXES
   lib64 lib
-)
+  )
 
 target_link_libraries(hiop_cuda INTERFACE
   ${CUDA_cublas_LIBRARY}
@@ -44,7 +44,7 @@ target_link_libraries(hiop_cuda INTERFACE
   ${CUDA_cublasLt_LIBRARY}
   ${CUDA_nvblas_LIBRARY}
   ${CUDA_culibos_LIBRARY}
-)
+  )
 
 target_include_directories(hiop_cuda INTERFACE ${CUDA_TOOLKIT_INCLUDE})
 
