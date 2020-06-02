a723fc1bd2cb64ecb286dfd51fbfcee5dd532ca5
Peles, Slaven <slaven.peles@pnnl.gov>
2020-06-02 10:04:39 -0700

    Resolved small issues with matrix unit tests addressed in #21 Also added tests specific to hiopMatrixDense

--- tests/testMatrix.cpp
+++ tests/testMatrix.cpp
@@ -1,3 +1,58 @@
+// Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory (LLNL).
+// Written by Cosmin G. Petra, petra1@llnl.gov.
+// LLNL-CODE-742473. All rights reserved.
+//
+// This file is part of HiOp. For details, see https://github.com/LLNL/hiop. HiOp 
+// is released under the BSD 3-clause license (https://opensource.org/licenses/BSD-3-Clause). 
+// Please also read “Additional BSD Notice” below.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+// i. Redistributions of source code must retain the above copyright notice, this list 
+// of conditions and the disclaimer below.
+// ii. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the disclaimer (as noted below) in the documentation and/or 
+// other materials provided with the distribution.
+// iii. Neither the name of the LLNS/LLNL nor the names of its contributors may be used to 
+// endorse or promote products derived from this software without specific prior written 
+// permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+// SHALL LAWRENCE LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+// AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Additional BSD Notice
+// 1. This notice is required to be provided under our contract with the U.S. Department 
+// of Energy (DOE). This work was produced at Lawrence Livermore National Laboratory under 
+// Contract No. DE-AC52-07NA27344 with the DOE.
+// 2. Neither the United States Government nor Lawrence Livermore National Security, LLC 
+// nor any of their employees, makes any warranty, express or implied, or assumes any 
+// liability or responsibility for the accuracy, completeness, or usefulness of any 
+// information, apparatus, product, or process disclosed, or represents that its use would
+// not infringe privately-owned rights.
+// 3. Also, reference herein to any specific commercial products, process, or services by 
+// trade name, trademark, manufacturer or otherwise does not necessarily constitute or 
+// imply its endorsement, recommendation, or favoring by the United States Government or 
+// Lawrence Livermore National Security, LLC. The views and opinions of authors expressed 
+// herein do not necessarily state or reflect those of the United States Government or 
+// Lawrence Livermore National Security, LLC, and shall not be used for advertising or 
+// product endorsement purposes.
+
+/**
+ * @file testMatrix.cpp
+ * 
+ * @author Asher Mancinelli <asher.mancinelli@pnnl.gov>, PNNL
+ * @author Slaven Peles <slaven.peles@pnnl.gov>, PNNL
+ * 
+ */
 #include <iostream>
 #include <cassert>
 
@@ -19,6 +74,8 @@ int main(int argc, char** argv)
     if(0 == rank)
         printf("Support for MPI is enabled\n");
 #endif
+    if(argc > 1)
+        std::cout << "Executable " << argv[0] << " doesn't take any input.";
 
     global_ordinal_type M_local = 5 * numRanks;
     global_ordinal_type K_local = 2 * M_local;
@@ -55,17 +112,13 @@ int main(int argc, char** argv)
         hiop::hiopMatrixDense B_mxn(M_global, N_global, n_partition, comm);
         hiop::hiopMatrixDense A_nxm(N_global, M_global, m_partition, comm);
         hiop::hiopMatrixDense A_nxn(N_global, N_global, n_partition, comm);
-        hiop::hiopMatrixDense B_nxn(N_global, N_global, n_partition, comm);
         hiop::hiopMatrixDense A_kxn(K_global, N_global, n_partition, comm);
-        hiop::hiopMatrixDense A_kxk(K_global, K_global, k_partition, comm);
-        hiop::hiopMatrixDense A_mxk(M_global, K_global, k_partition, comm);
         hiop::hiopMatrixDense A_mxm(M_global, M_global, m_partition, comm);
         hiop::hiopMatrixDense A_kxm(K_global, M_global, m_partition, comm);
 
         // Some matrices need to scale only in one dimension as
-        // ranks scale. The subscripted size will denote which
-        // dimension will not scale with ranks by suffixing an
-        // 'l' to the dimension.
+        // ranks scale. The subscripted size suffixed with an 'l'
+        // indicates which dimension will not scale with ranks.
         hiop::hiopMatrixDense A_klxm(K_local, M_global, m_partition, comm);
         hiop::hiopMatrixDense A_mlxk(M_local, K_global, k_partition, comm);
 
@@ -80,7 +133,6 @@ int main(int argc, char** argv)
         //
         // Distributed vectors
         hiop::hiopVectorPar x_n_dist(N_global, n_partition, comm);
-        hiop::hiopVectorPar x_m_dist(M_global, m_partition, comm);
 
         // Local vectors
         hiop::hiopVectorPar x_n_nodist(N_global);
@@ -105,6 +157,10 @@ int main(int argc, char** argv)
 #ifdef HIOP_DEEPCHECKS
             fail += test.matrixAssertSymmetry(A_nxn, rank);
 #endif
+
+            // specific to matrixTestsDense
+            fail += test.matrixCopyBlockFromMatrix(A_mxm, A_kxn, rank);
+            fail += test.matrixCopyFromMatrixBlock(A_kxn, A_mxm, rank);
         }
 
         fail += test.matrixTransTimesMat(A_mxk_local, A_kxn, A_mxn, rank);
@@ -114,6 +170,20 @@ int main(int argc, char** argv)
         fail += test.matrixIsFinite(A_mxn, rank);
         fail += test.matrixNumRows(A_mxn, M_global, rank);
         fail += test.matrixNumCols(A_mxn, N_global, rank);
+
+        // specific to matrixTestsDense
+        fail += test.matrixCopyFrom(A_mxn, B_mxn, rank);
+
+        // This matrix is allocated only for this test, since it requires
+        // more rows to be allocated with the constructor. Here we allocate
+        // one extra row for this purpose.
+        hiop::hiopMatrixDense A_mxn_extra_row(M_global, N_global, n_partition, comm, M_global+1);
+        fail += test.matrixAppendRow(A_mxn_extra_row, x_n_dist, rank);
+        fail += test.matrixCopyRowsFrom(A_kxn, A_mxn, rank);
+        fail += test.matrixCopyRowsFromSelect(A_mxn, A_kxn, rank);
+        fail += test.matrixShiftRows(A_mxn, rank);
+        fail += test.matrixReplaceRow(A_mxn, x_n_dist, rank);
+        fail += test.matrixGetRow(A_mxn, x_n_dist, rank);
     }
 
     // Test RAJA matrix
@@ -132,6 +202,10 @@ int main(int argc, char** argv)
         std::cout << "Matrix tests passed\n";
       }
     }
+
+    delete[] m_partition;
+    delete[] n_partition;
+    delete[] k_partition;
 
 #ifdef HIOP_USE_MPI
     MPI_Finalize();
