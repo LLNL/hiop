ca55bbc7135664e795f6fb503cb431f61ec14f0d
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-11-25 18:11:02 -0800

    Pipeline trigger fix (#126)
    * adds pipeline trigger git push action
    
    * Adding abs path to openblas on marianas
    
    Co-authored-by: pelesh <slaven.peles@pnnl.gov>

--- BUILD.sh
+++ BUILD.sh
@@ -176,6 +176,7 @@ marianas|dl*)
     export MY_METIS_DIR=$APPS_DIR/metis/$MY_METIS_VERSION
     export MY_HIOP_MAGMA_DIR=$APPS_DIR/magma/2.5.2/cuda10.2
     export MY_COINHSL_DIR=$PROJ_DIR/$MY_CLUSTER/ipopt
+    extra_cmake_args="$extra_cmake_args -DBLAS_LIBRARIES=/usr/lib64/libopenblas.so"
     ;;
 *)
     echo
