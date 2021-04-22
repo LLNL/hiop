850b515f20a5286f5b98a03571183860c2e6e615
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2021-04-22 09:54:36 -0700

    Update macros to use correct sublib names

--- cmake/HiOpAddComponent.cmake
+++ cmake/HiOpAddComponent.cmake
@@ -10,6 +10,31 @@ Relies on the following input defined variables:
 
 #]]
 
+# Ensure at least one library type is being built!
+if((NOT HIOP_BUILD_SHARED) AND (NOT HIOP_BUILD_STATIC))
+  message(WARNING "Must build at least one of SHARED or STATIC libraries!")
+  message(WARNING "Enabling HIOP_BUILD_STATIC.")
+  set(HIOP_BUILD_STATIC ON)
+endif()
+
+# This is the default library which HiOp::HiOp will point to. Try to use
+# shared by default.
+set(hiop_default_library_type "SHARED")
+if(NOT HIOP_BUILD_SHARED)
+  set(hiop_default_library_type "STATIC")
+endif()
+string(TOLOWER
+  ${hiop_default_library_type}
+  hiop_default_library_type_lower
+  )
+set(hiop_default_library_name
+  "hiop_${hiop_default_library_type_lower}"
+  )
+mark_as_advanced(FORCE
+  hiop_default_library_type
+  hiop_default_library_type_lower
+  )
+
 #[[
 
 @brief Add HiOp component library. All sources from component library will be
@@ -23,90 +48,60 @@ Relies on the following input defined variables:
 #]]
 macro(hiop_add_component)
   set(options "")
-  set(oneValueArgs "")
+  set(oneValueArgs COMPONENT_NAME)
   set(multiValueArgs SOURCES INTERFACE_HEADERS LINK_LIBRARIES)
   cmake_parse_arguments(hiop_add_component "${options}" "${oneValueArgs}"
                         "${multiValueArgs}" ${ARGN})
+  
+  message(STATUS "Configuring component library hiop_${hiop_add_component_COMPONENT_NAME}")
+  add_library(hiop_${hiop_add_component_COMPONENT_NAME}
+    OBJECT
+    ${hiop_add_component_SOURCES}
+    )
 
-  # We currently build all the sources only once, and reuse these object files
-  # for both shared and static libraries. If we add support for building for
-  # multiple compute devices (eg CUDA *and* HIP), we will need to create two
-  # of these object libraries.
-  if(NOT TARGET hiop_obj)
-    add_library(hiop_obj OBJECT ${hiop_add_component_SOURCES})
-  else()
-    target_sources(hiop_obj PRIVATE ${hiop_add_component_SOURCES})
+  if(hiop_add_component_LINK_LIBRARIES)
+    target_link_libraries(hiop_${hiop_add_component_COMPONENT_NAME}
+      ${hiop_add_component_LINK_LIBRARIES}
+      )
   endif()
 
-  target_link_libraries(hiop_obj ${hiop_add_component_LINK_LIBRARIES})
+  target_link_libraries(hiop_${hiop_add_component_COMPONENT_NAME}
+    PRIVATE
+    hiop_math
+    )
 
   # Install headers
-  install(FILES ${hiop_add_component_INTERFACE_HEADERS} DESTINATION include)
-endmacro()
-
+  if(hiop_add_component_INTERFACE_HEADERS)
+    install(FILES ${hiop_add_component_INTERFACE_HEADERS} DESTINATION include)
+  endif()
+endmacro(hiop_add_component)
 
 #[[
-
-@brief Creates extra alias libraries and ensures consistence between variabels.
-
-@post At least one of HIOP_BUILD_SHARED and HIOP_BUILD_STATIC is truthy
-@pre All component libraries have already been created.
-
-#]]
-macro(hiop_create_alias_library)
-  # This is the default library which HiOp::HiOp will point to. Try to use
-  # shared by default.
-  set(hiop_add_component_default_library "hiop_shared")
-  if(NOT HIOP_BUILD_SHARED)
-    set(hiop_add_component_default_library "hiop_static")
-  endif()
-
-  # Ensure at least one library type is being built!
-  if((NOT HIOP_BUILD_SHARED) AND (NOT HIOP_BUILD_STATIC))
-    message(FATAL_ERROR "Must build at least one of shared or static!")
-  endif()
-
-  # Create default library pointed to by HiOp::HiOp
-  if(NOT TARGET HiOp::HiOp)
-    add_library(HiOp::HiOp ALIAS ${hiop_add_component_default_library})
-    # Naked _hiop_ target is still used elsewhere in the codebase
-    add_library(hiop ALIAS ${hiop_add_component_default_library})
-  endif()
-endmacro()
-
-#[[
-
-@brief Creates concrete libraries (eg shared and/or static) from hiop_obj
-  target.
-
-@note Takes no parameters.
-@pre HIOP_BUILD_<libtype> variables must be set before this macro is called.
-  This macro depends on these variables to determine which libraries to create.
-@note Creates targets HiOp::SHARED and HiOp::STATIC (if appropriate options are
-  set) which allow a user to link against a particular version of libhiop from
-  their cmake code (once this code is appropriately exported).
-@note Calls hiop_configure_components macro.
 
 ]]
 macro(hiop_create_concrete_libraries)
+
   foreach(libtype shared static)
     string(TOUPPER ${libtype} libtype_upper)
-    if(${HIOP_BUILD_${libtype_upper}})
-      add_library(hiop_${libtype} ${libtype_upper} $<TARGET_OBJECTS:hiop_obj>)
+    if(${HIOP_BUILD_${libtype_upper}} AND NOT TARGET hiop_${libtype})
+      add_library(hiop_${libtype}
+        ${libtype_upper}
+        $<TARGET_OBJECTS:hiop_optimization>
+        $<TARGET_OBJECTS:hiop_utils>
+        $<TARGET_OBJECTS:hiop_linalg>
+        $<$<BOOL:${HIOP_BUILD_SHARED}>:$<TARGET_OBJECTS:hiop_interface>>
+        )
       add_library(HiOp::${libtype_upper} ALIAS hiop_${libtype})
-      target_include_directories(hiop_${libtype}
-        PUBLIC
-        $<TARGET_PROPERTY:hiop_obj,INTERFACE_INCLUDE_DIRECTORIES>
-        PRIVATE
-        $<TARGET_PROPERTY:hiop_obj,INCLUDE_DIRECTORIES>
-        )
-      target_link_libraries(hiop_${libtype}
-        PUBLIC
-        hiop_math
-        $<TARGET_PROPERTY:hiop_obj,LINK_LIBRARIES>
-        )
+      target_link_libraries(hiop_${libtype} PUBLIC hiop_math)
       set_target_properties(hiop_${libtype} PROPERTIES OUTPUT_NAME hiop)
       install(TARGETS hiop_${libtype} DESTINATION lib)
     endif()
   endforeach()
-endmacro()
+
+  # Create default library pointed to by HiOp::HiOp
+  add_library(HiOp::HiOp ALIAS ${hiop_default_library_name})
+
+  # Naked _hiop_ target is still used elsewhere in the codebase
+  add_library(hiop ALIAS ${hiop_default_library_name})
+
+endmacro(hiop_create_concrete_libraries)
