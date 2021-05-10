0ae6882b962f40ee29cb4164b17ec34433ab1e23
rcrutherford <robert.rutherford@pnnl.gov>
2021-05-09 17:43:56 -0700

    Treat CUDA as first class language in CMake
    modified:   CMakeLists.txt
                Using CUDA, C and CXX as the project languages
    modified:   cmake/FindHiopCudaLibraries.cmake
                Modifying to tread CUDA as first class lang.

--- cmake/FindHiopCudaLibraries.cmake
+++ cmake/FindHiopCudaLibraries.cmake
@@ -6,47 +6,13 @@ Exports target `hiop_cuda` which finds all cuda libraries needed by hiop.
 ]]
 
 add_library(hiop_cuda INTERFACE)
+target_link_libraries(hiop_cuda INTERFACE culibos)
+target_link_libraries(hiop_cuda INTERFACE cublasLt)
+target_link_libraries(hiop_cuda INTERFACE nvblas)
+target_link_libraries(hiop_cuda INTERFACE cusparse)
+target_link_libraries(hiop_cuda INTERFACE cudart)
 
-find_package(CUDA REQUIRED)
-
-find_library(CUDA_culibos_LIBRARY
-  NAMES
-  culibos
-  PATHS
-  ${CUDA_TOOLKIT_ROOT_DIR} $ENV{CUDA_TOOLKIT_ROOT_DIR}
-  PATH_SUFFIXES
-  lib64 lib
-  )
-
-find_library(CUDA_cublasLt_LIBRARY
-  NAMES
-  cublasLt
-  PATHS
-  ${CUDA_TOOLKIT_ROOT_DIR} $ENV{CUDA_TOOLKIT_ROOT_DIR}
-  PATH_SUFFIXES
-  lib64 lib
-  )
-
-find_library(CUDA_nvblas_LIBRARY
-  NAMES
-  nvblas
-  PATHS
-  ${CUDA_TOOLKIT_ROOT_DIR} $ENV{CUDA_TOOLKIT_ROOT_DIR}
-  PATH_SUFFIXES
-  lib64 lib
-  )
-
-target_link_libraries(hiop_cuda INTERFACE
-  ${CUDA_cublas_LIBRARY}
-  ${CUDA_CUDART_LIBRARY}
-  ${CUDA_cudadevrt_LIBRARY}
-  ${CUDA_cusparse_LIBRARY}
-  ${CUDA_cublasLt_LIBRARY}
-  ${CUDA_nvblas_LIBRARY}
-  ${CUDA_culibos_LIBRARY}
-  )
-
-target_include_directories(hiop_cuda INTERFACE ${CUDA_TOOLKIT_INCLUDE})
+target_include_directories(hiop_cuda INTERFACE ${CUDAToolkit_INCLUDE_DIRS})
 
 # for now we rely on MAGMA for GPUs computations
 include(FindHiopMagma)
