2ff7abf46ca1e7ca9409c24b7cbd2a7a7e556398
Asher Mancinelli <ashermancinelli@gmail.com>
2021-06-21 18:06:49 -0400

    Update ascent variables

--- scripts/ascentVariables.sh
+++ scripts/ascentVariables.sh
@@ -6,22 +6,26 @@ fi
 export MY_CLUSTER=ascent
 export PROJ_DIR=/gpfs/wolf/proj-shared/csc359
 source $PROJ_DIR/src/spack/share/spack/setup-env.sh
-module purge
-module load cuda/10.2.89
-module use $PROJ_DIR/$MY_CLUSTER/Modulefiles/Core
-module load exasgd-base
-module load gcc/7.4.0
-export CC=gcc CXX=g++ # For some reason this is not picked up by cmake with this module
-module load spectrum-mpi/10.3.1.2-20200121
-module load openblas/0.3.9-omp
-module load cmake/3.18.2
 
 ls $PROJ_DIR/src/spack/var/spack/environments/*
 
 spack env activate hiop-v0-4-3-deps
 
-export MY_NVCC_ARCH="70"
-export EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DHIOP_NVCC_ARCH=$MY_NVCC_ARCH"
+module purge
+module load cuda/11.0.2
+module load gcc/7.4.0
+module load spectrum-mpi/10.3.1.2-20200121
+module load cmake/3.18.2
+
+#These are not picked up by the module for some reason
+export CC=/sw/ascent/gcc/7.4.0/bin/gcc
+export CXX=/sw/ascent/gcc/7.4.0/bin/g++
+export FC=/sw/ascent/gcc/7.4.0/bin/gfortran
+
+export EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS \
+  -DCMAKE_CUDA_ARCHITECTURES=70 \
+  -DCUDA_TOOLKIT_ROOT_DIR=$CUDA_TOOLKIT_ROOT_DIR \
+  "
 
 if [[ ! -f $BUILDDIR/nvblas.conf ]]; then
   cat > $BUILDDIR/nvblas.conf <<-EOD
@@ -34,3 +38,4 @@ EOD
 fi
 export NVBLAS_CONFIG_FILE=$BUILDDIR/nvblas.conf
 EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DHIOP_TEST_WITH_BSUB=ON"
+module load cuda/11.0.2
