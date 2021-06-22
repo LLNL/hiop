1ee977ffc03987c9c212fc6295e061df726b6a4e
Asher Mancinelli <ashermancinelli@gmail.com>
2021-06-21 17:51:15 -0700

    Update marianas moduel use commands

--- scripts/marianasVariables.sh
+++ scripts/marianasVariables.sh
@@ -7,18 +7,8 @@ export APPS_DIR=/share/apps
 
 #  NOTE: The following is required when running from Gitlab CI via slurm job
 source /etc/profile.d/modules.sh
-(
-module use -a /share/apps/modules/Modules/versions
-module use -a $MODULESHOME/modulefiles/environment
-module use -a $MODULESHOME/modulefiles/development/mpi
-module use -a $MODULESHOME/modulefiles/development/mlib
-module use -a $MODULESHOME/modulefiles/development/compilers
-module use -a $MODULESHOME/modulefiles/development/tools
-module use -a $MODULESHOME/modulefiles/apps
-module use -a $MODULESHOME/modulefiles/libs
 module use -a /qfs/projects/exasgd/src/spack/share/spack/modules/linux-centos7-broadwell/
 module use -a /qfs/projects/exasgd/src/spack/share/spack/modules/linux-centos7-x86_64/
-) 2>&1 1>&/dev/null
 
 export NVBLAS_CONFIG_FILE=$PROJ_DIR/$MY_CLUSTER/nvblas.conf
 
