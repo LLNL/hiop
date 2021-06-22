db3593654952e61b728f624b0ad59ac40f31d02f
Asher Mancinelli <ashermancinelli@gmail.com>
2021-06-21 17:13:09 -0700

    Update PNNL systems to use modules

--- scripts/marianasVariables.sh
+++ scripts/marianasVariables.sh
@@ -5,6 +5,7 @@ export MY_CLUSTER="marianas"
 export PROJ_DIR=/qfs/projects/exasgd
 export APPS_DIR=/share/apps
 export SPACK_ARCH=linux-centos7-broadwell
+
 #  NOTE: The following is required when running from Gitlab CI via slurm job
 source /etc/profile.d/modules.sh
 (
@@ -19,10 +20,38 @@ module use -a $MODULESHOME/modulefiles/libs
 module use -a $PROJ_DIR/src/spack/share/spack/modules/$SPACK_ARCH/
 ) 2>&1 1>&/dev/null
 
-source $PROJ_DIR/src/spack/share/spack/setup-env.sh
-spack env activate hiop-v0-4-4-deps-marianas
 export NVBLAS_CONFIG_FILE=$PROJ_DIR/$MY_CLUSTER/nvblas.conf
 
+# Load spack-built modules
+module load exasgd-arpack-ng/3.8.0/openmpi-3.1.3/gcc-7.3.0-zirdafa
+module load exasgd-autoconf-archive/2019.01.06/gcc-7.3.0-bzoccad
+module load exasgd-blt/0.3.6/gcc-7.3.0-yvfnprq
+module load exasgd-butterflypack/1.2.1/openmpi-3.1.3/gcc-7.3.0-2syic2u
+module load exasgd-camp/0.1.0/cuda-10.2.89/gcc-7.3.0-bvsl36m
+module load exasgd-cmake/3.20.2/gcc-7.3.0-ymp2go7
+module load exasgd-coinhsl/2015.06.23/gcc-7.3.0-rkvwgny
+module load exasgd-cub/1.12.0-rc0/gcc-7.3.0-o5omfqk
+module load exasgd-cuda/10.2.89/gcc-7.3.0-fytuxuh
+module load exasgd-gmp/6.2.1/gcc-7.3.0-uimke6b
+module load exasgd-ipopt/3.12.10/gcc-7.3.0-kyxesp7
+module load exasgd-magma/2.5.4/cuda-10.2.89/gcc-7.3.0-25cv4mc
+module load exasgd-metis/5.1.0/gcc-7.3.0-232rotu
+module load exasgd-mpfr/4.1.0/gcc-7.3.0-3rrse4o
+module load exasgd-ncurses/6.2/gcc-7.3.0-h2zctno
+module load exasgd-netlib-scalapack/2.1.0/openmpi-3.1.3/gcc-7.3.0-kcsvzxm
+module load exasgd-openblas/0.3.5/gcc-7.3.0-ug3gdtf
+module load exasgd-openmpi/3.1.3/gcc-7.3.0-etzhtfg
+module load exasgd-parmetis/4.0.3/openmpi-3.1.3/gcc-7.3.0-4atunu3
+module load exasgd-perl/5.26.0/gcc-7.3.0-wog3ysl
+module load exasgd-pkgconf/1.7.4/gcc-7.3.0-n2z4txg
+module load exasgd-raja/0.13.0/cuda-10.2.89/gcc-7.3.0-jemkyui
+module load exasgd-strumpack/5.1.1/cuda-10.2.89/openmpi-3.1.3/gcc-7.3.0-4fqr6cn
+module load exasgd-suite-sparse/5.10.1/gcc-7.3.0-kj6x5bx
+module load exasgd-texinfo/6.5/gcc-7.3.0-l44ipkv
+module load exasgd-umpire/4.1.2/cuda-10.2.89/gcc-7.3.0-3uphncx
+module load exasgd-zfp/0.5.5/gcc-7.3.0-h3p6smb
+
+# Load system modules
 module load gcc/7.3.0
 module load cuda/10.2.89
 module load openmpi/3.1.3
--- scripts/newellVariables.sh
+++ scripts/newellVariables.sh
@@ -13,9 +13,30 @@ module use -a /share/apps/modules/compilers
 module use -a /share/apps/modules/mpi
 module use -a /etc/modulefiles
 
-# Activate spack environment
-source $PROJ_DIR/src/spack/share/spack/setup-env.sh
-spack env activate hiop-v0-4-2-deps-newell
+# Load spack-built modules
+module load exasgd-arpack-ng/3.8.0/openmpi-3.1.5/gcc-7.4.0-yl7lsxq
+module load exasgd-autoconf-archive/2019.01.06/gcc-7.4.0-zr3h7p2
+module load exasgd-blt/0.3.6/gcc-7.4.0-z4yhwmy
+module load exasgd-butterflypack/1.2.1/openmpi-3.1.5/gcc-7.4.0-iaw77e6
+module load exasgd-camp/0.1.0/cuda-10.2.89-system/gcc-7.4.0-fvkaniz
+module load exasgd-coinhsl/2015.06.23/gcc-7.4.0-qhwhtdc
+module load exasgd-cub/1.12.0-rc0/gcc-7.4.0-5lcegaf
+module load exasgd-gmp/6.2.1/gcc-7.4.0-6svtfvr
+module load exasgd-ipopt/3.12.10/gcc-7.4.0-sfzycsu
+module load exasgd-magma/2.5.4/cuda-10.2.89-system/gcc-7.4.0-bikffe2
+module load exasgd-metis/5.1.0/gcc-7.4.0-7cjo5kb
+module load exasgd-mpfr/4.1.0/gcc-7.4.0-bkusb67
+module load exasgd-netlib-scalapack/2.1.0/openmpi-3.1.5/gcc-7.4.0-3uycg6f
+module load exasgd-openblas/0.3.5/gcc-7.4.0-72rh6zx
+module load exasgd-parmetis/4.0.3/openmpi-3.1.5/gcc-7.4.0-ym6nkdq
+module load exasgd-perl/5.26.0/gcc-7.4.0-j742qcl
+module load exasgd-pkgconf/1.7.4/gcc-7.4.0-5ios5aw
+module load exasgd-raja/0.13.0/cuda-10.2.89-system/gcc-7.4.0-uj7zop4
+module load exasgd-strumpack/5.1.1/cuda-10.2.89-system/openmpi-3.1.5/gcc-7.4.0-x55sdhv
+module load exasgd-suite-sparse/5.8.1/gcc-7.4.0-ij4zlyb
+module load exasgd-texinfo/6.5/gcc-7.4.0-r24oezq
+module load exasgd-umpire/4.1.1/cuda-10.2.89-system/gcc-7.4.0-njlsagd
+module load exasgd-zfp/0.5.5/gcc-7.4.0-av4beua
 
 # Load system modules
 module load gcc/7.4.0
@@ -23,5 +44,5 @@ module load cuda/10.2
 module load openmpi/3.1.5
 module load cmake/3.19.6
 
-export MY_NVCC_ARCH="70"
+export EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DCMAKE_CUDA_ARCHITECTURES=70"
 export NVBLAS_CONFIG_FILE=$PROJ_DIR/$MY_CLUSTER/nvblas.conf
