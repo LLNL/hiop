efed73612854b252fd5575231647406f0c70983f
Nai-Yuan Chiang <chiang7@llnl.gov>
2021-08-30 09:29:36 -0700

    Sparse gpu kernel dev 1 (#301)
    * RAJA copyRowsFrom
    
    loop through nnz instead of rows in some RAJA code
    
    * add method to hiopVectorInt; unit test for CopyRowsFrom
    
    * update copyRowsFrom; Add copy_rows_block_from
    
    * remove RAJA::inclusive_scan
    
    * use index_type and size_type in new functions
    
    Co-authored-by: Chiang <chiang7@sephiroth.llnl.gov>
    Co-authored-by: Nai-Yuan Chiang <chiang7@login1.ascent.olcf.ornl.gov>

--- tests/testVector.cpp
+++ tests/testVector.cpp
@@ -346,7 +346,11 @@ int runIntTests(const char* mem_space)
   fail += test.vectorGetElement(*x);
   fail += test.vectorSetElement(*x);
 
+  auto* y = LinearAlgebraFactory::create_vector_int(mem_space, sz);
+  fail += test.vector_copy_from(*x, *y);
+  
   delete x;
+  delete y;
 
   return fail;
 }
