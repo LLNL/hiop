d348485926effc459d2da54c3962d802da2de9e8
Nai-Yuan Chiang <chiang7@llnl.gov>
2021-10-22 09:22:17 -0700

    update matlab matrix reader for the input files without rhs and sol (#346)
    Co-authored-by: Chiang <chiang7@sephiroth.llnl.gov>

--- src/LinAlg/load_kkt_mat.m
+++ src/LinAlg/load_kkt_mat.m
@@ -13,8 +13,22 @@ nnz=A(5); % number of nnz of matrix (or of the upper triangle for symmetric matr
 rowp = A(6:m+6); % row pointers in colidx and vals (see below)
 colidx=A(m+7:nnz+m+6); % column indexes for each nz
 vals=A(nnz+m+7:nnz+nnz+m+6); % values for each nz
-rhs=A(nnz+nnz+m+7:nnz+nnz+m+m+6); % rhs 
-sol=A(nnz+nnz+m+m+7:nnz+nnz+m+m+m+6); % solution
+has_rhs = true;
+try
+    rhs=A(nnz+nnz+m+7:nnz+nnz+m+m+6); % rhs
+catch
+    warning('Cannot find rhs from the file. Set rhs as a vector of ones.');
+    rhs = ones(m,1);
+    has_rhs = false;
+end
+has_sol = true;
+try
+    sol=A(nnz+nnz+m+m+7:nnz+nnz+m+m+m+6); % solution
+catch
+    warning('Cannot find sol from the file.');
+    has_sol = false;
+end
+
 fprintf('Loaded a matrix of size %d with %d nonzeros from "%s"\n', ...
     m, nnz, filename);
 
@@ -45,6 +59,8 @@ end
     
 %% solve using Matlab and check the residuals of the Matlab solution 
 %% and of the solution from .iajaaa file
-sol2 = M\rhs;
-fprintf('residuals norm: Matlab sol %.5e   sol from file: %.5e\n', ...
-    norm(M* sol2 - rhs), norm(M* sol - rhs));
+if has_sol == true
+    sol2 = M\rhs;
+    fprintf('residuals norm: Matlab sol %.5e   sol from file: %.5e\n', ...
+        norm(M* sol2 - rhs), norm(M* sol - rhs));
+end
