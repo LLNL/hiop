0a16054a4aa652f13b3063323939802991884ef7
nychiang <sorakid507@gmail.com>
2022-06-09 16:23:28 -0700

    correct scripts

--- scripts/quartzVariables.sh
+++ scripts/quartzVariables.sh
@@ -2,8 +2,8 @@ module load gcc/8.3.1 cmake/3.18 mkl/2019.0 mvapich2/2.3
 #export LD_LIBRARY_PATH=/g/g92/chiang7/workspace/software/quartz/COIN-OR/build/lib:$LD_LIBRARY_PATH
 #export PATH=/g/g92/chiang7/workspace/software/quartz/COIN-OR/build/bin:$PATH
 
-EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DHIOP_COINHSL_DIR:STRING=/g/g92/chiang7/workspace/software/quartz/COIN-OR/build"
-
+EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DHIOP_COINHSL_DIR:STRING=/usr/workspace/chiang7/software/quartz/COIN-OR/build"
+EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DHIOP_METIS_DIR:STRING=/usr/workspace/chiang7/software/quartz/COIN-OR/build"
 export CMAKE_CACHE_SCRIPT=gcc-cpu.cmake
 
 
