da8f9f7e166563d95a7a750915b8074fe4aa5848
Nai-Yuan Chiang <chiang7@llnl.gov>
2022-06-14 12:43:49 -0700

    Create LLNL Gitlab CI (#492)
    * LLNL ci tests

---
+++ scripts/quartzVariables.sh
@@ -0,0 +1,7 @@
+module load gcc/8.3.1 cmake/3.18 mkl/2019.0 mvapich2/2.3
+
+EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DHIOP_COINHSL_DIR:STRING=/usr/workspace/chiang7/software/quartz/COIN-OR/build"
+EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DHIOP_METIS_DIR:STRING=/usr/workspace/chiang7/software/quartz/COIN-OR/build"
+export CMAKE_CACHE_SCRIPT=gcc-cpu.cmake
+
+
