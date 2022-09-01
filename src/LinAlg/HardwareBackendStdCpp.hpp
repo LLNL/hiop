71f7e106a1a097bdbfed60d7c84b1daee43c2b74
Cosmin G. Petra <petra1@llnl.gov>
2022-09-01 16:30:43 -0400

    misc

--- src/LinAlg/HardwareBackendStdCpp.hpp
+++ src/LinAlg/HardwareBackendStdCpp.hpp
@@ -7,4 +7,12 @@ struct FeatureIsPresent<MemBackendCpp>
  static constexpr bool value = true;
 };
 
+template<class MemBackendCpp>
+struct SupportsHostMemSpace
+{
+  static constexpr bool value = true;
+};
+
+
+  
 } // end namespace hiop
