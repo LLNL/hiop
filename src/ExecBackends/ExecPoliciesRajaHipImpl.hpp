794b1d4982877413a31aa65b94faf894705c00de
Cosmin G. Petra <petra1@llnl.gov>
2022-09-19 16:25:18 -0400

    added Hip and Cuda RAJA Impl files

---
+++ src/ExecBackends/ExecPoliciesRajaHipImpl.hpp
@@ -0,0 +1,105 @@
+// Copyright (c) 2022, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory (LLNL).
+// LLNL-CODE-742473. All rights reserved.
+//
+// This file is part of HiOp. For details, see https://github.com/LLNL/hiop. HiOp 
+// is released under the BSD 3-clause license (https://opensource.org/licenses/BSD-3-Clause). 
+// Please also read "Additional BSD Notice" below.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+// i. Redistributions of source code must retain the above copyright notice, this list 
+// of conditions and the disclaimer below.
+// ii. Redistributions in binary form must reproduce the above copyright notice, 
+// this list of conditions and the disclaimer (as noted below) in the documentation and/or 
+// other materials provided with the distribution.
+// iii. Neither the name of the LLNS/LLNL nor the names of its contributors may be used to 
+// endorse or promote products derived from this software without specific prior written 
+// permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+// SHALL LAWRENCE LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+// AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Additional BSD Notice
+// 1. This notice is required to be provided under our contract with the U.S. Department 
+// of Energy (DOE). This work was produced at Lawrence Livermore National Laboratory under 
+// Contract No. DE-AC52-07NA27344 with the DOE.
+// 2. Neither the United States Government nor Lawrence Livermore National Security, LLC 
+// nor any of their employees, makes any warranty, express or implied, or assumes any 
+// liability or responsibility for the accuracy, completeness, or usefulness of any 
+// information, apparatus, product, or process disclosed, or represents that its use would
+// not infringe privately-owned rights.
+// 3. Also, reference herein to any specific commercial products, process, or services by 
+// trade name, trademark, manufacturer or otherwise does not necessarily constitute or 
+// imply its endorsement, recommendation, or favoring by the United States Government or 
+// Lawrence Livermore National Security, LLC. The views and opinions of authors expressed 
+// herein do not necessarily state or reflect those of the United States Government or 
+// Lawrence Livermore National Security, LLC, and shall not be used for advertising or 
+// product endorsement purposes.
+
+/**
+ * @file ExecPoliciesRajaHipImpl.hpp
+ *
+ * @author Cosmin G. Petra <petra1@llnl.gov>, LLNL
+ * @author Slaven Peles <peless@ornl.gov>, ORNL
+ * @author Nai-Yuan Chiang <chiang7@llnl.gov>, LLNL
+ */
+
+/**
+ * This file contains HIP RAJA policies. Should be generally included only in HIP 
+ * compilation units.
+ */
+
+#ifndef HIOP_EXEC_POL_RAJA_HIP
+#define HIOP_EXEC_POL_RAJA_HIP
+
+#if defined(HIOP_USE_RAJA) && defined(HIOP_USE_HIP)
+
+#include "ExecSpace.hpp"
+
+#include <hip.h>
+#include <RAJA/RAJA.hpp>
+
+namespace hiop
+{
+
+#define RAJA_LAMBDA [=] __device__
+  
+template<>
+struct ExecRajaPoliciesBackend<ExecPolicyRajaHip>
+{
+  static constexpr unsigned short int HIOP_RAJA_GPU_BLOCK_SIZE = 128;
+  
+  using hiop_raja_exec   = RAJA::hip_exec<HIOP_RAJA_GPU_BLOCK_SIZE>;
+  using hiop_raja_reduce = RAJA::hip_reduce;
+  using hiop_raja_atomic = RAJA::hip_atomic;
+
+  // The following are primarily for _matrix_exec_
+  using hiop_block_x_loop = RAJA::hip_block_x_loop;
+  using hiop_thread_x_loop = RAJA::hip_thread_x_loop;
+  template<typename T>
+  using hiop_kernel = RAJA::statement::HipKernel<T>;
+
+  using matrix_exec =
+    RAJA::KernelPolicy<
+      hiop_kernel<
+        RAJA::statement::For<1, hiop_block_x_loop,
+          RAJA::statement::For<0, hiop_thread_x_loop,
+            RAJA::statement::Lambda<0>
+          >
+        >
+      >
+    >;
+};
+} //end of namespace hiop
+#endif // defined(HIOP_USE_RAJA) && defined(HIOP_USE_HIP)
+  
+#endif
