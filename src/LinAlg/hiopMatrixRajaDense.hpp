d1ad5820688c2099788a668fd166b8cb13ddd526
Cosmin G. Petra <petra1@llnl.gov>
2022-12-19 19:40:12 -0500

    added temporary define for raja dense matrix  for OMP

--- src/LinAlg/hiopMatrixRajaDense.hpp
+++ src/LinAlg/hiopMatrixRajaDense.hpp
@@ -7,7 +7,12 @@
 
 namespace hiop
 {
-
+//todo: remove this file, update LA factory
+#if defined(HIOP_USE_CUDA)
   using hiopMatrixRajaDense = hiopMatrixDenseRaja<MemBackendUmpire, ExecPolicyRajaCuda>;
+#else
+  using hiopMatrixRajaDense = hiopMatrixDenseRaja<MemBackendUmpire, ExecPolicyRajaOmp>;
+#end
+
 
 } // namespace hiop
