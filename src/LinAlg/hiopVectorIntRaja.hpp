10e414df98c1db24c3d1ba5b66b08bd8345743db
Cosmin G. Petra <petra1@llnl.gov>
2023-01-30 14:16:11 -0500

    added methods for copying to/from seq vector int

--- src/LinAlg/hiopVectorIntRaja.hpp
+++ src/LinAlg/hiopVectorIntRaja.hpp
@@ -95,12 +95,9 @@ public:
   
   virtual void copy_from(const index_type* v_local);
 
-  virtual void copy_from_vectorseq(const hiopVectorIntSeq& src)
-  {
-  }
-  virtual void copy_to_vectorseq(hiopVectorIntSeq& src) const
-  {
-  }
+  virtual void copy_from_vectorseq(const hiopVectorIntSeq& src);
+
+  virtual void copy_to_vectorseq(hiopVectorIntSeq& dest) const;
 
   
   /// @brief Set all elements to zero.
