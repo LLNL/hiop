4937c9f74f98ac24fba8798d38bb60cd27e7222e
nychiang <sorakid507@gmail.com>
2023-05-03 18:05:10 -0700

    fix

--- src/LinAlg/hiopVectorPar.cpp
+++ src/LinAlg/hiopVectorPar.cpp
@@ -246,13 +246,13 @@ void hiopVectorPar::startingAtCopyFromStartingAt(int start_idx_dest,
                                                  int start_idx_src)
 {
   size_type howManyToCopyDest = this->n_local_ - start_idx_dest;
+  const hiopVectorPar& v = dynamic_cast<const hiopVectorPar&>(v_in);
 
 #ifdef HIOP_DEEPCHECKS
   assert(n_local_==n_ && "only for local/non-distributed vectors");
   assert(v.n_local_==v.n_ && "only for local/non-distributed vectors");
 #endif
   assert((start_idx_dest>=0 && start_idx_dest<this->n_local_) || this->n_local_==0);
-  const hiopVectorPar& v = dynamic_cast<const hiopVectorPar&>(v_in);
   assert((start_idx_src>=0 && start_idx_src<v.n_local_) || v.n_local_==0 || v.n_local_==start_idx_src);
   size_type howManyToCopySrc = v.n_local_-start_idx_src;
 
