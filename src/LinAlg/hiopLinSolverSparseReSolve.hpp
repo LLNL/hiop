28440fa3e9dfb60db39333b02e8c0dd4f482ead9
Slaven Peles <peless@ornl.gov>
2023-08-11 20:01:51 -0400

    Remove commented out code.

--- src/LinAlg/hiopLinSolverSparseReSolve.hpp
+++ src/LinAlg/hiopLinSolverSparseReSolve.hpp
@@ -155,28 +155,6 @@ protected:
   template <typename T> void hiopCheckCudaError(T result, const char* const file, int const line);
 };
 
-// class hiopLinSolverSymSparseReSolveGPU : public hiopLinSolverSymSparseReSolve
-// {
-// public:  
-//   hiopLinSolverSymSparseReSolveGPU(const int& n, const int& nnz, hiopNlpFormulation* nlp);
-//   virtual ~hiopLinSolverSymSparseReSolveGPU();
-
-//   virtual int matrixChanged();
-//   virtual bool solve(hiopVector& x_);
-
-//   /** Multiple rhs not supported yet */
-//   virtual bool
-//   solve(hiopMatrix& /* x */)
-//   {
-//     assert(false && "not yet supported");
-//     return false;
-//   }
-
-// private:
-//   hiopVector* rhs_host_;
-//   hiopMatrixSparse* M_host_;
-// };
-
 } // namespace hiop
 
 
